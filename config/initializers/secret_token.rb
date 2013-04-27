6fc01f2402832275036a2d41547363b6f0ca6059
Steven <stevenr2d2@gmail.com>
2013-04-27 15:30:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaHack::Application.config.secret_token = 'ae22ff8ce257a7a336c9b4fe8c69fb600a92f9e322dd619681b699dc79477319da274f57e664284cdc92ca74b7e63072a75d9ca1133f9ddddc88567fc6864bef'
